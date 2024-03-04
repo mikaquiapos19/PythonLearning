71f3d44c141c57d0a61f3b180d32ee4448f41070
mikaquiapos19 <mikaquiapos19@gmail.com>
2024-03-04 14:04:16 +0000

    Displaying images (with surfaces) and text

--- pygame/pygamelearn.py
+++ pygame/pygamelearn.py
@@ -5,7 +5,11 @@ pygame.init() #initiates pygame
 screen = pygame.display.set_mode((800, 400)) #creating a display surface - parameter -tuple: width, height
 pygame.display.set_caption('Test Game')
 clock = pygame.time.Clock() #regulates fps for the game
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50) #(font type, font size)
 
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Black') #(text, AA, colour) #False for AA because of pixel art. Otherwise, make it True
 
 while True:
     for event in pygame.event.get(): #loops through events; get events
@@ -14,5 +18,10 @@ while True:
             exit() 
     #draw all our elements
     #update everything
+
+    screen.blit(sky_surface, (0,0)) #block image transfer (surface, pos) pos = left, top
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300, 50)) #for text
+
     pygame.display.update() #updates the display surface 
     clock.tick(60) #game running at 60fps minimum 
