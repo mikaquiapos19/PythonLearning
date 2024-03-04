bffecb731bb526dff49ff4e416aedd2ec851386f
mikaquiapos19 <mikaquiapos19@gmail.com>
2024-03-04 13:10:06 +0000

    Creating a blank window (+ clock)

---
+++ pygamelearn.py
@@ -0,0 +1,18 @@
+import pygame #tutorial from Clear Code
+from sys import exit #closes any kind of code once you call it
+
+pygame.init() #initiates pygame
+screen = pygame.display.set_mode((800, 400)) #creating a display surface - parameter -tuple: width, height
+pygame.display.set_caption('Test Game')
+clock = pygame.time.Clock() #regulates fps
+
+
+while True:
+    for event in pygame.event.get(): #loops through events; get events
+        if event.type == pygame.QUIT: #allows us to close window
+            pygame.quit() #un initialise pygame
+            exit() 
+    #draw all our elements
+    #update everything
+    pygame.display.update() #updates the display surface 
+    clock.tick(60) #game running at 60fps minimum.
